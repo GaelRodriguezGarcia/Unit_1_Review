f01590b35574450fc5de1ec00d669401d7fc9f21
Gael Rodriguez Garcia <grodriguez236@west-mec.org>
2020-01-09 18:27:08 -0700

    linked html and made first function

---
+++ tny_timer.js
@@ -0,0 +1,23 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Review Assignment
+
+   Event Timer
+   Author: Gael Rodriguez Garcia
+   Date:  1/9/20 
+
+*/
+function showClock(){
+    var thisDay = Date("May 19, 2018, 9:31:27 a.m ")
+    var localDate
+}
+
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
